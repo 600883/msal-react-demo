2daf49d229ae5ee34787fcfb497b6047cc2ea305
sivertsaeter <sivertseter@yahoo.com>
2024-02-13 11:16:26 +0100

    added msal login functionality

--- src/index.js
+++ src/index.js
@@ -8,12 +8,22 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 
+import { PublicClientApplication } from '@azure/msal-browser';
+
+const pca = new PublicClientApplication({
+    auth: {
+        clientId: '0e188131-7628-48a9-aede-ee0ced19c163',
+        authority: 'https://login.microsoftonline.com/0a1d7995-b734-4d29-8b90-ba9a075bdd6d',
+        redirectUri: '/',
+    }
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <ThemeProvider theme={theme}>
-                <App />
+                <App msalInstance={pca}/>
             </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>
