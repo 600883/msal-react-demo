6e7b102f50edefdbd03151720c442af0b826f476
sivertsaeter <sivertseter@yahoo.com>
2024-02-21 10:13:37 +0100

    small fix

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,12 +1,10 @@
-import { BorderColor } from '@mui/icons-material';
-import MyLoader from "./Loader";
 import './CustomButton.css';
 
-function CustomButton({startLoader, stopLoader }) {
+function CustomButton() {
     
     return (
         <div className='buttonContainer'>
-            <button className='button1' onClick={startLoader}>I am a button</button>
+            <button className='button1'>I am a button</button>
             <button className='button2'>I am a button2</button>
             <button className='button3'>I am a button3</button>
             <button className='button4'>I am a button4</button>
